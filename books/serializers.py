eff99ed7658f7ecbb6d01788061bdcede6dd5100
Danyal Faheem <danyal.faheem@arbisoft.com>
2023-10-16 11:13:38 +0500

    Removed Comments

--- books/serializers.py
+++ books/serializers.py
@@ -29,23 +29,6 @@ class BookCreateSerializer(serializers.ModelSerializer):
 class FullRequestSerializer(serializers.ModelSerializer):
     """Serializer for Request Model that allows all actions"""
 
-    # def validate(self, attrs):
-    #     """
-    #     Validates if user has finished their quota of 3 issued books or
-    #     If all copies of the request book are issued or
-    #     User can already requested or issued the requested book
-    #     """
-    #     if attrs['user'].issued_books() >= 3:
-    #         raise serializers.ValidationError(
-    #             "Max issued books at once can't be more than 3.")
-    #     elif attrs['book'].remaining_count() <= 0:
-    #         raise serializers.ValidationError(
-    #             "All copies of this book are currently issued.")
-    #     elif len(Request.objects.filter(user=attrs['user'], book=attrs['book'], status=Request.Status.ISSUED)) > 0 and attrs['status'] == Request.Status.ISSUED:
-    #         raise serializers.ValidationError(
-    #             "Book has already been issued to this user")
-    #     return super().validate(attrs)
-
     class Meta:
         model = Request
         fields = ['id', 'user', 'book', 'status', 'issue_date', 'return_date']
@@ -65,24 +48,6 @@ class ReadRequestSerializer(serializers.ModelSerializer):
 class UserRequestSerializer(serializers.ModelSerializer):
     """Serializer for Requests allowed to the User"""
 
-    # def validate(self, attrs):
-    #     """
-    #     Validates if user has finished their quota of 3 issued books or
-    #     If all copies of the request book are issued or
-    #     User can already requested or issued the requested book
-    #     """
-    #     print(attrs)
-    #     if self.request.user.issued_books() >= 3:
-    #         raise serializers.ValidationError(
-    #             "Max issued books at once can't be more than 3.")
-    #     elif attrs['book'].remaining_count() <= 0:
-    #         raise serializers.ValidationError(
-    #             "All copies of this book are currently issued.")
-    #     elif len(Request.objects.filter(user=attrs['user'], book=attrs['book'], status=Request.Status.ISSUED)) > 0 and attrs['status'] == Request.Status.ISSUED:
-    #         raise serializers.ValidationError(
-    #             "Book has already been issued to this user")
-    #     return super().validate(attrs)
-
     class Meta:
         model = Request
         fields = ['id', 'user', 'book', 'status']
@@ -92,19 +57,6 @@ class UserRequestSerializer(serializers.ModelSerializer):
 
 class FullTicketSerializer(serializers.ModelSerializer):
     """Serializer for Ticket Model"""
-
-    # def validate(self, attrs):
-    #     """
-    #     Validates if the requested book is already in the library or
-    #     If the user has already requested this book beforehand
-    #     """
-    #     if len(Book.objects.filter(name=attrs['name'], author=attrs['author'])) > 0 and attrs['status'] == Ticket.Status.REQUESTED:
-    #         raise serializers.ValidationError(
-    #             "This book already exists in the library")
-    #     elif len(Ticket.objects.filter(user=attrs['user'], name=attrs['name'], author=attrs['author'])) > 0 and attrs['status'] == Ticket.Status.REQUESTED:
-    #         raise serializers.ValidationError(
-    #             "You have already Requested this book!")
-    #     return super().validate(attrs)
 
     class Meta:
         model = Ticket
@@ -121,19 +73,6 @@ class ReadTicketSerializer(serializers.ModelSerializer):
 
 class UserTicketSerializer(serializers.ModelSerializer):
     """Serializer for Ticket Model"""
-
-    # def validate(self, attrs):
-    #     """
-    #     Validates if the requested book is already in the library or
-    #     If the user has already requested this book beforehand
-    #     """
-    #     if len(Book.objects.filter(name=attrs['name'], author=attrs['author'])) > 0 and attrs['status'] == Ticket.Status.REQUESTED:
-    #         raise serializers.ValidationError(
-    #             "This book already exists in the library")
-    #     elif len(Ticket.objects.filter(user=attrs['user'], name=attrs['name'], author=attrs['author'])) > 0 and attrs['status'] == Ticket.Status.REQUESTED:
-    #         raise serializers.ValidationError(
-    #             "You have already Requested this book!")
-    #     return super().validate(attrs)
 
     class Meta:
         model = Ticket
